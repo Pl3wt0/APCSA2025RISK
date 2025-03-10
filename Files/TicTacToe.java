f305df580ff37aa54ae34d15cea29bfaf2bcd286
Pl3wt0 <lucas.rosen.1078@gmail.com>
2025-03-10 19:34:40 -0400

    Fixed checkWin Method of TicTacToe

--- Files/TicTacToe.java
+++ Files/TicTacToe.java
@@ -19,23 +19,30 @@ public class TicTacToe extends BoardGame implements Playable{
         
     }
 
-    public boolean checkWin(){
+    public boolean checkWin(char player){
         boolean win = false;
         for(int i = 0;i<board.length;i++){
             if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] == board[i][2]){
-                return true;
+                if(player == board[i][0]){
+                    return true;
+                }
+                
             }
         }
         for(int i = 0; i<board[0].length;i++){
             if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] == board[2][i]){
-                return true;
+                if(player == board[0][i]){
+                    return true;
+                }
             }
         }
         if(board[0][0] == board[1][1]&&board[1][1]==board[2][2]&&board[0][0]==board[1][1]){
-            return true;
-        }else{
+            if(player == board[0][0]){
+                return true;
+            }
+        
             return false;
-        }
+        
     }
     
 }
