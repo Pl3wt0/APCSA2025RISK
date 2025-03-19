d3eb2b8ae5c3e1771cd08f2adc3b8fbe9f1c2572
blakrr <blakeyman5@gmail.com>
2025-03-19 11:49:41 -0400

    card logic commented

--- Files/Card.java
+++ Files/Card.java
@@ -3,4 +3,12 @@ public class Card {
     private boolean horse;
     private boolean cannon;
     private boolean troop; 
+
+    // logic
+    // three horse
+    // three cannon
+    // three troop
+    // one of each kind
+    // any two cards and a joker
+    // any one card and two jokers
 }
