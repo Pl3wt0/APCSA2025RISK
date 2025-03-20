3351872a4ec3db824ba2fa3c6ce108511dad024e
Pl3wt0 <lucas.rosen.1078@gmail.com>
2025-03-20 09:47:47 -0400

    Created BoardManager Class

---
+++ Files/BoardManager.java
@@ -0,0 +1,8 @@
+package Files;
+import java.util.ArrayList;
+
+public class BoardManager {
+    ArrayList <Player> players = new ArrayList<Player>();
+    ArrayList <Territory> territories = new ArrayList<Territory>();
+    
+}
