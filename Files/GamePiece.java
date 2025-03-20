25f710fe8240a635ea3eb4aca3e62ce8a3cc849a
Pl3wt0 <lucas.rosen.1078@gmail.com>
2025-03-20 14:22:06 -0400

    Made gamePiece Constructor

--- Files/GamePiece.java
+++ Files/GamePiece.java
@@ -1,5 +1,11 @@
 package Files;
 
 public class GamePiece {
+    private String color;
+    private Integer playerNum;
 
+    public GamePiece(String color, Integer playerNum){
+        this.color = color;
+        this.playerNum = playerNum;
+    }
 }
