8d39d0eb8fa5b2397a079c3dfdbc668353a12649
Pl3wt0 <lucas.rosen.1078@gmail.com>
2025-03-21 09:41:13 -0400

    Added Comments and drawCard()

--- Files/Player.java
+++ Files/Player.java
@@ -8,11 +8,36 @@ public class Player {
     private String playerName = "";
     private Integer playerNum = null;
 
+    /**
+     * Constructor for Player object
+     * 
+     * @param String name of the player
+     * @param Integer number to identify the player
+     */
     public Player(String name, Integer playerNum){
         this.playerNum = playerNum;
         playerName = name;
     }
 
+    
+    
+    
+    /** 
+     * Adds a Card to hand 
+     * Calls drawCard() from deck to get a random card
+     * Only adds a Card if hand has less than 5 Card objects
+     * 
+     * @param deck
+     * @return boolean
+     */
+    public boolean addCard(Deck deck){
+        if(hand.size()<5){
+            hand.add(deck.drawRandom());
+            return true;
+        } else{
+            return false;
+        }
+    }
     
 
 }
