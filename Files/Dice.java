69e3596cf045f8cf46662d002e9ddf606c2f5aab
Pl3wt0 <lucas.rosen.1078@gmail.com>
2025-03-25 13:16:56 -0400

    Wrote dice class

--- Files/Dice.java
+++ Files/Dice.java
@@ -1,5 +1,23 @@
 package Files;
 
 public class Dice {
+    private Integer sides=0;
 
+    /**
+     * Constructs dice object with specific number of sides
+     * 
+     * @param sides number of sides for the dice object
+     */
+    public Dice(Integer sides){
+        this.sides = sides;
+    }
+
+    /**
+     * Method to roll the dice with the specifc number of sides
+     * 
+     * @return Integer result of the dice roll
+     */
+    public Integer rollDice(){
+        return (int)(Math.random()*sides)+1;
+    }
 }
