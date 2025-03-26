0c48dd43bc9ecb684c3936c341cb01571823b147
nregnault6 <nicolasaregnault@gmail.com>
2025-03-26 11:52:34 -0400

    Add continent class

---
+++ Files/Continent.java
@@ -0,0 +1,7 @@
+public class Continent {
+    private List<Territory> territories;
+    private int bonusTroops;
+
+    public List<Territory> getTerritories() { return territories; }
+    public int getBonusTroops() { return bonusTroops; }
+}
