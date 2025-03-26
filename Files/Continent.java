919a1eb7cbfdb08b5ee31ec3e99bdfe99da9c3db
nregnault6 <nicolasaregnault@gmail.com>
2025-03-26 11:55:21 -0400

    updated Continent class

--- Files/Continent.java
+++ Files/Continent.java
@@ -2,6 +2,11 @@ public class Continent {
     private List<Territory> territories;
     private int bonusTroops;
 
+    public Continent(List<Territory> territories, int bonusTroops) {
+        this.territories = territories;
+        this.bonusTroops = bonusTroops;
+    }
+
     public List<Territory> getTerritories() { return territories; }
     public int getBonusTroops() { return bonusTroops; }
 }
